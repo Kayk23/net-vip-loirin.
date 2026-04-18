15fc4223c07215e0e568612047c16c0f7ff2a232
Kayk23 <marqueskayky81@gmail.com>
2026-04-17 23:11:05 -0300

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,21 +2,20 @@ export default {
   async fetch(request) {
     const host = request.headers.get('Host');
 
-    // MIRA GRUDADA + SEM RECOIL
     if (host.includes('config.uca.cloud.unity3d.com')) {
       const configVip = {
         "performance": {
           "enabled": true,
-          "headshot_scale": 45.0,    // Força magnética na cabeça
-          "hitbox_scale": 1.3,       // Evita que a mira "atropele" a cabeça
-          "recoil_scale": 0.0,       // Zero Recoil (Arma não treme)
-          "spread_scale": 0.0,       // Bala não espalha
-          "near_hit_bias": "head"    // Garante HS mesmo colado no inimigo
+          "headshot_scale": 45.0,
+          "hitbox_scale": 1.3,
+          "recoil_scale": 0.0,
+          "spread_scale": 0.0,
+          "near_hit_bias": "head"
         },
         "aimbot": {
-          "aim_lock": "head_only",   // Ignora o peito
-          "fov_radius": 20.0,        // Raio de atração curto pra ser discreto e eficiente
-          "smooth": 0.1              // Puxada instantânea
+          "aim_lock": "head_only",
+          "fov_radius": 20.0,
+          "smooth": 0.1
         }
       };
       return new Response(JSON.stringify(configVip), {
@@ -24,7 +23,6 @@ export default {
       });
     }
 
-    // BLOQUEIO DE BAN (Logs da Garena)
     if (host.includes('log') || host.includes('analytics')) {
       return new Response(null, { status: 403 });
     }
